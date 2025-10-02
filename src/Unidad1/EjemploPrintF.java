121954b55f033d9f85df901eabe1dd5ad425c6d3
efamine <aminftri@gmail.com>
2025-10-02 11:08:54 +0200

    commit de 2 de octubre

---
+++ src/Unidad1/EjemploPrintF.java
@@ -0,0 +1,15 @@
+package Unidad1;
+
+public class EjemploPrintF { 
+	
+	public static void main(String[] args) {
+		
+		// Ejemplo de printf con números enteros y saldo de línea.
+		System.out.printf("La mitd de %d es %d \n y suman %d. \n",42, 21, 63);
+		System.out.print("Fin");
+		
+		// Ejemplo de printf con números double
+		double precio = 5.75;
+		System.out.printf("El precio es: %f", precio);
+	}
+}
